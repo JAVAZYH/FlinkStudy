a8f1f9c012c14275aa0dd3eae7b2da26a1260616
zhangyuhang <1534280186@qq.com>
2020-05-29 10:51:59 +0800

    第一次测试提交

---
+++ src/main/scala/test.scala
@@ -0,0 +1,26 @@
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.datastream.DataStream
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object test {
+  def main(args: Array[String]): Unit = {
+    // 从外部命令中获取参数
+    val params: ParameterTool =  ParameterTool.fromArgs(args)
+    val host: String = params.get("host")
+    val port: Int = params.getInt("port")
+
+    // 创建流处理环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    // 接收socket文本流
+    val textDstream = env.socketTextStream(host, port)
+    import org.apache.flink.api.scala._
+//    val dataStream: DataStream[(String, Int)] = textDstream.flatMap(_.split("\\s")).filter(_.nonEmpty).map((_, 1)).keyBy(0).sum(1)
+
+    textDstream.print().setParallelism(1)
+
+    // 启动executor，执行任务
+    env.execute()
+
+  }
+
+}
